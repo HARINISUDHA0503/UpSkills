101ade2f3bdb774a21281f5dff868aecd52e80d8
HARINISUDHA0503 <115806019+HARINISUDHA0503@users.noreply.github.com>
2023-11-28 15:52:57 +0530

    Add files via upload

---
+++ department-service/src/main/java/com/harini/departmentservice/service/DepartmentService.java
@@ -0,0 +1,9 @@
+package com.harini.departmentservice.service;
+
+import com.harini.departmentservice.dto.DepartmentDto;
+
+public interface DepartmentService {
+    DepartmentDto saveDepartment(DepartmentDto departmentDto);
+
+    DepartmentDto getDepartmentByCode(String code);
+}
